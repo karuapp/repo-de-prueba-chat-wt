49fd9cfeba45dee70e6c3d75994bb954723cf29f
karuapp <104691902+karuapp@users.noreply.github.com>
2025-03-07 16:57:36 -0600

    12

--- backend/src/routes/n8nRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import isAuth from "../middleware/isAuth";
-import * as N8nController from "../controllers/N8nController";
-
-const n8nRoutes = Router();
-
-n8nRoutes.get("/listworkFlow", isAuth, N8nController.listWrkFlow);
-
-export default n8nRoutes;
